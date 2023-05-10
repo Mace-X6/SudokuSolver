c2fcedd00562c5d5146db5121e28045d282c16a8
Mace-X6 <mees@vansteijn.info>
2023-05-10 17:05:01 +0200

    can now also print the array before it is solved

--- SudokuSolver/PrintField.cs
+++ SudokuSolver/PrintField.cs
@@ -2,10 +2,10 @@
 
 public class PrintField
 {
-    public static void Print(List<Cell> cells)
+    public static void PrintCells(List<Cell> cells)
     {
         string output = "";
-        string separator = ">-----------------------<\n";
+        string separator = ">-------+-------+-------<\n";
         for (int i = 0; i < 9; i++)
         {
             if (i % 3 == 0)
@@ -25,4 +25,33 @@ public class PrintField
         Console.WriteLine(output);
 
     }
+    
+    public static void PrintValues(int[] values)
+    {
+        string output = "";
+        string separator = ">-------+-------+-------<\n";
+        List<string> stringValues = new List<string>();
+        for (int i = 0; i < values.Length; i ++){
+            stringValues.Add(values[i] == 0 ? " " : values[i].ToString());
+        }
+        for (int i = 0; i < 9; i++)
+        {
+            if (i % 3 == 0)
+            {
+                output += separator;
+            }
+            // for (int j = 0; j < 9; j ++){
+            //     field 
+            // }
+
+            int cellId = i*9;
+            output += $"| {stringValues[cellId]} {stringValues[cellId + 1]} {stringValues[cellId + 2]} | {stringValues[cellId + 3]} {stringValues[cellId + 4]} {stringValues[cellId + 5]} | {stringValues[cellId + 6]} {stringValues[cellId + 7]} {stringValues[cellId + 8]} |\n";
+            if (i == 8){
+                output += separator;
+            }
+        }
+        Console.WriteLine(output);
+
+    }
+
 }
