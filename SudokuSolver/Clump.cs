c311c2bd18d494fa853d33a801a84d82fdc7ee14
Mace-X6 <mees@vansteijn.info>
2023-05-11 13:16:45 +0200

    formatting

--- SudokuSolver/Clump.cs
+++ SudokuSolver/Clump.cs
@@ -3,16 +3,21 @@
 public class Clump
 {
     public Cell[] Cells { get; }
-    
-    public Clump(Cell[] cells){
+
+    public Clump(Cell[] cells)
+    {
         Cells = cells;
-        foreach (Cell cell in Cells){
+        foreach (Cell cell in Cells)
+        {
             cell.AssignedMethod += UpdateCells;
         }
     }
-    private void UpdateCells(object? sender, CellValueChangedEvent cellValueChangedEvent){
-        foreach (Cell cell in Cells){
-            if (cell.Id != cellValueChangedEvent.CellId){
+    private void UpdateCells(object? sender, CellValueChangedEvent cellValueChangedEvent)
+    {
+        foreach (Cell cell in Cells)
+        {
+            if (cell.Id != cellValueChangedEvent.CellId)
+            {
                 cell.RemoveAvailableOption(cellValueChangedEvent.NewValue);
             }
         }
