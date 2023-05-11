0fc1b0a71ee5ce4ab86cf9f67a6d3a3b69b1955e
Mace-X6 <mees@vansteijn.info>
2023-05-11 09:49:21 +0200

    added clumps field

--- SudokuSolver/Grid.cs
+++ SudokuSolver/Grid.cs
@@ -4,6 +4,7 @@ public class Grid
     public List<Clump> Rows { get; private set; }
     public List<Clump> Columns { get; private set; }
     public List<Clump> Blocks { get; private set; }
+    public List<Clump> Clumps { get; private set; }
     public List<Cell> Cells { get; private set; }
     public Grid()
     {
@@ -11,6 +12,7 @@ public class Grid
         Rows = new List<Clump>();
         Columns = new List<Clump>();
         Blocks = new List<Clump>();
+        Clumps = new List<Clump>();
 
         for (int id = 0; id < 81; id++)
         {
@@ -20,10 +22,17 @@ public class Grid
 
         for (int j = 0; j < 9; j++)
         {
+            var row = CellDistribute.Row(Cells, j);
+            var column = CellDistribute.Column(Cells, j);
+            var block = CellDistribute.Block(Cells, j);
+            Rows.Add(row);
+            Columns.Add(column);
+            Blocks.Add(block);
+            Clumps.Add(row);
+            Clumps.Add(column);
+            Clumps.Add(block);
 
-            Rows.Add(CellDistribute.Row(Cells, j));
-            Columns.Add(CellDistribute.Column(Cells, j));
-            Blocks.Add(CellDistribute.Block(Cells, j));
+
         }
     }
 
