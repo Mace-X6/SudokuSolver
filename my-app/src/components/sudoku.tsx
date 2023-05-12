46e6ce4876bcdf25f0b74bb74e2c672aad5d3080
Guidodeg <26955830+Guidodeg@users.noreply.github.com>
2023-05-12 15:58:30 +0200

    Add confetti, difficulty and randomized numbers

--- my-app/src/components/sudoku.tsx
+++ my-app/src/components/sudoku.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Confetti from 'react-confetti';
 
 type PuzzleCell = {
     id: number;
@@ -6,16 +7,17 @@ type PuzzleCell = {
     options: number[];
 };
 type Puzzle = PuzzleCell[];
+type Difficulty = 'Easy' | 'Medium' | 'Hard' | 'Expert';
 
 const SudokuPage: React.FC = () => {
     const [puzzle, setPuzzle] = useState<Puzzle | null>([]);
+    const [isSolved, setIsSolved] = useState<boolean>(false);
+    const [difficulty, setDifficulty] = useState<Difficulty>('Easy');
 
     const fetchPuzzle = async (endpoint: string) => {
         try {
-          const response = await fetch(`Sudoku/${endpoint}`);
+          const response = await fetch(`Sudoku/${endpoint}?difficulty=${difficulty}`);
           const data = await response.json();
-          
-         console.log(data)
           setPuzzle(data.cells);
         } catch (error) {
           console.error('Error:', error);
@@ -32,38 +34,78 @@ const SudokuPage: React.FC = () => {
                 body: JSON.stringify(puzzle?.map(x => x.value))
             });
             const data = await response.json();
-            setPuzzle(data.cells);
+            gradualUpdate(data);
         } catch (error) {
             console.error('Error:', error);
         }
+    };
+
+    const gradualUpdate = async (data: any) => {
+        const newCells = data.cells;
+        // Create an array of indices and shuffle it
+        const indices = Array.from({ length: newCells.length }, (_, i) => i);
+        for (let i = indices.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [indices[i], indices[j]] = [indices[j], indices[i]];
+        }
+    
+        // Update the cells in the order specified by the shuffled indices array
+        for (const i of indices) {
+            await new Promise(r => setTimeout(r, 50));
+            setPuzzle(oldCells => [...(oldCells as Puzzle).slice(0, i), newCells[i], ...(oldCells as Puzzle).slice(i+1)]);
+        }
+        setIsSolved(data.isSolved);
     };
 
     useEffect(() => {
         fetchPuzzle('GetPuzzle');
     }, []);
 
+    const handleDifficultyChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setDifficulty(event.target.value as Difficulty);
+    };
     const handleSolve = () => {
         postPuzzle('SolvePuzzle');
     };
 
     const handleReset = () => {
+        setIsSolved(false);
         fetchPuzzle('GetPuzzle');
     };
 
     return (
         <div>
           <h1>Sudoku Puzzle</h1>
-          <div style={{ display: 'grid', border: '1px solid white', backgroundColor: 'white', gridTemplateColumns: 'repeat(9, 1fr)', gap: '1px' }}>
-            {puzzle && puzzle.sort((a, b) => a.id - b.id).map((cell) => (
+          <div style={{ display: 'grid', border: '1px solid white', backgroundColor: 'white', gridTemplateColumns: 'repeat(9, 1fr)', gap: '2px' }}>
+            {puzzle && puzzle.map((cell) => (
               <div key={cell.id} style={{ backgroundColor: 'black', padding: '1px' }}>
                 {cell.value === 0 ? '' : cell.value}
               </div>
             ))}
+             {isSolved && <Confetti />}
           </div>
+          <div>
+                <label>
+                    <input type="radio" value="Easy" checked={difficulty === 'Easy'} onChange={handleDifficultyChange} />
+                    Easy
+                </label>
+                <label>
+                    <input type="radio" value="Medium" checked={difficulty === 'Medium'} onChange={handleDifficultyChange} />
+                    Medium
+                </label>
+                <label>
+                    <input type="radio" value="Hard" checked={difficulty === 'Hard'} onChange={handleDifficultyChange} />
+                    Hard
+                </label>
+                <label>
+                    <input type="radio" value="Expert" checked={difficulty === 'Expert'} onChange={handleDifficultyChange} />
+                    Expert
+                </label>
+            </div>
+          <button onClick={handleReset}>GET PUZZLE</button>
           <button onClick={handleSolve}>SOLVE</button>
-          <button onClick={handleReset}>RESET</button>
         </div>
-      );
+    );
 };
 
 export default SudokuPage;
