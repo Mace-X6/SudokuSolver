46ad1e3c398a01ca915f2d91bec7795adc667be0
Mace-X6 <mees@vansteijn.info>
2023-05-12 11:04:52 +0200

    if there is only one cell in a clump with a certain option, that cell should take that option as its value.

---
+++ SudokuSolver/Solver/UniqueOptionInClumpStrategy.cs
@@ -0,0 +1,27 @@
+namespace SudokuSolver.Solver;
+class UniqueOptionInClumpStrategy : ISudokuSolverStrategy
+{
+    public bool Solve(Grid grid)
+    {
+        bool madeChanges = false;
+        foreach (Clump clump in grid.Clumps)
+        {
+                madeChanges |= SetUniqueOption(clump);
+        }
+        return madeChanges;
+    }
+    private bool SetUniqueOption(Clump clump){
+        var cells = clump.Cells;
+            foreach (Cell cell in clump.Cells)
+            {
+                foreach (int availableOption in cell.AvailableOptions){
+                    if (cells.Count(c => c.Id != cell.Id && c.AvailableOptions.Contains(availableOption)) == 0){
+                        //available option is unique ->
+                        cell.SetValue(availableOption);
+                        return true;
+                    }
+                }
+            }
+            return false;
+    }
+}
