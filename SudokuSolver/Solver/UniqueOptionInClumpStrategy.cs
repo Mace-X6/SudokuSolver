b4e790e25bcfd99e9dd36fa2e6705600ff2dd2d8
Mace-X6 <mees@vansteijn.info>
2023-05-12 11:58:34 +0200

    fixed bug where UniqueOptionInClumpStrategy would get stuck in infinite loop

--- SudokuSolver/Solver/UniqueOptionInClumpStrategy.cs
+++ SudokuSolver/Solver/UniqueOptionInClumpStrategy.cs
@@ -6,22 +6,28 @@ public class UniqueOptionInClumpStrategy : ISudokuSolverStrategy
         bool madeChanges = false;
         foreach (Clump clump in grid.Clumps)
         {
-                madeChanges |= SetUniqueOption(clump);
+            madeChanges |= SetUniqueOption(clump);
         }
         return madeChanges;
     }
-    private bool SetUniqueOption(Clump clump){
+    private bool SetUniqueOption(Clump clump)
+    {
         var cells = clump.Cells;
-            foreach (Cell cell in clump.Cells)
+        foreach (Cell cell in clump.Cells)
+        {
+            if (!cell.IsSolved)
             {
-                foreach (int availableOption in cell.AvailableOptions){
-                    if (cells.Count(c => c.Id != cell.Id && c.AvailableOptions.Contains(availableOption)) == 0){
+                foreach (int availableOption in cell.AvailableOptions)
+                {
+                    if (cells.Count(c => c.Id != cell.Id && c.AvailableOptions.Contains(availableOption)) == 0)
+                    {
                         //available option is unique ->
                         cell.SetValue(availableOption);
                         return true;
                     }
                 }
             }
-            return false;
+        }
+        return false;
     }
 }
